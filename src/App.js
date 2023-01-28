aec4155355a07e082f1c268122814c1cac3c836b
Hilary Goh <hilary.goh@gmail.com>
2023-01-28 17:52:49 +0800

    removed ocr add numpad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Webcam from "react-webcam";
 
 import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 import Button from '@mui/material/Button';
 import Fab from '@mui/material/Fab';
@@ -23,6 +23,7 @@ import { AttendanceBook } from './models';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+///// App CSV Download
 Amplify.configure(config);
 
 
@@ -79,13 +80,11 @@ function parse_response(json_response) {
   return result;
 }
 
-// App Bit
+///// App Bit
 
-const image_dim = {width: 400, height: 500};
-//crops total h w of image capture box
-
+//changes style of 'Thankyou' modal box
 const style = {
-//changes style of 'That's Wrong' dialog box
+
   position: 'absolute',
   top: '50%',
   left: '50%',
@@ -97,183 +96,98 @@ const style = {
   p: 5,
 };
 
-//function App({ signOut, user }) {
-  function App() {
+function App() {
+  //handle input
+  const [textSrc, setTextSrc] = React.useState("");
 
-  const webcamRef = React.useRef(null);
-
-  const [imgSrc, setImgSrc] = React.useState("https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg?w=204&h=204");
-  const [textSrc, setTextSrc] = React.useState(null);
-
-  const [openModal, setOpenModal] = React.useState(false);
-  
-  const [showVideoFeed, setShowVideoFeed] = React.useState(true);
-
-  const handleOpenModal = () => setOpenModal(true);
-  
-  const handleCloseModal = () => {
-    setOpenModal(false);
-    clearstuff();
-  }
-
-  const capture = React.useCallback(() => {
-
-    const imageSrc = preprocess(
-      webcamRef.current.getScreenshot(image_dim));
-    
-    setImgSrc(imageSrc);
-    console.log(imageSrc);
-    setShowVideoFeed(false);
- 
-    fetch('https://hf.space/embed/tomofi/EasyOCR/+/api/predict/', 
-      { method: "POST", 
-        body: JSON.stringify({ "data": [ imageSrc , ["en"] ]})
-       , headers: { "Content-Type": "application/json" } }
-     ).then(function(response) 
-         { return response.json(); }
-         ).then(
-          function(json_response)
-          { 
-          setTextSrc(parse_response(json_response));
-         });
-    
-  }, [webcamRef, setImgSrc]);
-
-  const clearstuff = () => {
-    setTextSrc("");
-    setImgSrc("https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg?w=204&h=204");
-    setShowVideoFeed(true);
-    console.log("cleared stuff ran!")
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setTextSrc(value);
   };
 
-  const save_and_clear = () => {
-    
+  const [openModal, setOpenModal] = React.useState(false);
+  const handleOpenModal = () => {
+    setOpenModal(true);
+    setTimeout(() => {
+      handleCloseModal();
+    }, 3000);
+  }
+
+
+  const save_and_clear = (e) => {
+    e.preventDefault();
+    handleOpenModal();
     if (textSrc) {
       save_number(textSrc);
       console.log("Sending textSrc")
       console.log("text src");
     };
     console.log("cleared");
-
-    clearstuff();
   };
-  
 
-/*
- var amplify_stuff = (
-  <div>
-  <Typography variant="h1">Hello {user.username}</Typography>
-    <Button onClick={signOut}>Sign out</Button>
-    <h2>Amplify Todos</h2>
-    </div>
- );*/
+  //handle modal
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setTextSrc("");
 
- var videoFeedSx;
- var screenshotSx;
+  };
 
- if (showVideoFeed) 
- {
-  videoFeedSx = [];
-  screenshotSx = { display: 'none' };
- } else
- {
-  videoFeedSx = { display: 'none' };
-  screenshotSx = [];
- }
-
- // UI Stuff
+ ///// UI Stuff
   return (
   <div>
     <Stack 
     mt={2}
     spacing={1}
     alignItems="center">
-    
 
+    <Container>
+    <Box     
+    component="img"
+        sx={{
+          height: 150,
+        }}
+        src="https://www.lakemongershed.org.au/uploads/9/3/0/7/93073146/lake-monger-community-shed-logo-352px_orig.png">
+    </Box>
+    </Container>
 
     <Container sx={{ width: '100%' }}>
-     <Typography variant="h5" gutterBottom>
-        Lake Monger Community Shed
-        Attendance Book
+     <Typography variant="h4" gutterBottom>
+       Please log your attendance here, no need to sign out.
      </Typography>
      <Divider />
     </Container>
-    <Divider />
+    <Divider/>
 
-    <Container >
-        <Typography variant="h4" gutterBottom>
-          Membership #:{ textSrc }
-        </Typography>
-    </Container>
-
-    <Container sx={screenshotSx}>
-    <Stack spacing={1} direction="row">
-        <Button 
-        sx={{ width: '100%' }} 
-        onClick={save_and_clear} 
-        variant="contained" 
-        color="success" 
-        size="large">
-          That's correct
-        </Button>
-        <Button 
-        sx={{ width: '100%' }} 
-        onClick={handleOpenModal} 
-        variant="outlined" 
-        color="error" 
-        size="large">
-          That's wrong
-        </Button>
-        <Modal
-          open={openModal}
-          onClose={handleCloseModal}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h4">
-              Please scan your badge again
-            </Typography>
-            <Typography id="modal-modal-description" variant="h5" sx={{ mt: 1}}>
-              Make sure it's not upside down.
-            </Typography>
-          </Box>
-        </Modal>
-        
-      </Stack>
-    </Container>
-    <Divider />
- 
-    <Container sx={videoFeedSx} >
-        <Webcam 
-          ref={webcamRef} 
-          screenshotFormat={'image/jpeg'}
-          screenshotQuality={0.1}
-          imageSmoothing={false}
-          videoConstraints={image_dim}
-        />
-    </Container>
-
-    <Container sx={screenshotSx} >
-        <img 
-          width={300} height={500}
-          src={imgSrc}
-        />
-    </Container>
-
-    
     <Container>
-       <Fab sx={{ width: '100%' }}
-            color='primary' 
-            variant="extended" 
-            onClick={capture}>
-            <Typography variant="h5">          
-            <CameraAltOutlinedIcon sx={{ fontSize:"20px", mx:2 }}/>
-            Please Scan Your Badge
-            </Typography>
-      </Fab>
+    <Box>
+      <form onSubmit={save_and_clear}>
+        <TextField 
+        value={textSrc}
+        onChange={handleInputChange}
+        type='number' fullWidth label="Enter your Membership Number" id="fullWidth"
+        inputProps={{ style: { fontSize: "5rem" } }}/>
+      </form>
+    </Box>
     </Container>
-    
+
+    <Container>
+      <Modal
+        open={openModal}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </Container>
+
     <Container>
        <Fab sx={{ width: '100%' }}
             color='secondary' 
@@ -282,6 +196,7 @@ const style = {
             Download Logs
       </Fab>
     </Container>
+
     </Stack>
     </div>
   );
